064a226b1608a44a10303511fb8ef0d5f7f6800c
LemonaInc <jaxonstevens@lemonainc.com>
2017-09-06 20:11:39 -0700

    Updated Views

--- MapViewController.swift
+++ MapViewController.swift
@@ -19,23 +19,19 @@ class MapViewController: UIViewController {
         
         // Set the longitude for the location
         var vancouver = CLLocationCoordinate2DMake (49.2827, -123.11934)
-        
         // Create the annotation
         var vancouverAnnotation = MKPointAnnotation()
         vancouverAnnotation.coordinate = vancouver
         vancouverAnnotation.title = "Vancouver, BC"
         vancouverAnnotation.subtitle = "Canada"
-        
+        // Set the span of the coordinate
         var span = MKCoordinateSpanMake(0.2,0.2)
         // Create the region for the initial mapview
         var region = MKCoordinateRegion(center: vancouver, span: span)
-        
         // Set the initial map region to the region defined
         Map.setRegion(region, animated: true)
-        
         // Add the annotation to the MapView
         Map.addAnnotation(vancouverAnnotation)
-        
         
         
     }
